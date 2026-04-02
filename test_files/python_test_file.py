f72fd9962c4dd5f36a1d0bd1f945ea510dce6606
samkelemen <samkelemen17@gmail.com>
2026-04-02 10:15:26 -0500

    Add autopairs plugin

--- test_files/python_test_file.py
+++ test_files/python_test_file.py
@@ -1,1 +1,9 @@
+# test_files/python_test_file.py
+def for_loop(a: int, b: int):
+    for i in range(a):
+        print(b)
+        print("Hello my name is Sam!")
 
+
+if __name__ == "__main__":
+    for_loop(10, 10)
